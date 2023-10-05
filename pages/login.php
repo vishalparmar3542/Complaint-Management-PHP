b89807f6a843abd6e6a3468491ebf1ea89697d1a
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2023-10-05 20:17:41 +0530

    created login page.

---
+++ pages/login.php
@@ -0,0 +1,92 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/home.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+	<div class="blob-container">
+    <div class="blob"></div>
+    <div class="blob one"></div>
+    <div class="blob two"></div>
+    <div class="blob three"></div>
+    <div class="blob four"></div>
+    <div class="blob five"></div>
+    <div class="blob six"></div>
+    <div class="blob seven"></div>
+    <div class="blob eight"></div>
+    <div class="blob nine"></div>
+    <div class="blob ten"></div>
+  </div>
+
+  <section>
+    <div class="card">
+              <div class="title">
+         <h1><span>Complaint Management</span> System</p>
+       </div>
+      <div class="description">
+      <div class="login-form">
+        <h2>Login</h2>
+        <form>
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" placeholder="Enter your username" required>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      <!-- <button>Join</button> -->
+        <!-- <p>Each color has its own psychological effects on individuals. Would you like to create an environment of stimulation or whet people’s appetite? Join us to examine the effects of colors!         </p> -->
+    </div>
+    
+      <!-- <div class="title">
+         <h1><span>Complaint Management</span> System</p>
+       </div> -->
+    </div>
+
+  </section>
+</div>
+
+</body>
+</html>
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/home.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="left-side">
+        <h2>Welcome to Complaint Management System</h2>
+        <img src="your-image.jpg" alt="Company Logo">
+    </div>
+    <div class="login-container">
+        <h1>Login</h1>
+        <form class="login-form" action="#" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    </div>
+</body>
+</html> -->
