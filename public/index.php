c8cd24d684177f15c697ec07aee99cec693f0711
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2023-10-05 19:23:00 +0530

    Basic structure.

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<script>
+    console.log("Hello from php website");
+</script>
+
+<?php
+    echo "Hello, world";
+        header("Location: http://localhost/project/pages/home.php");
+        exit();
+?>
